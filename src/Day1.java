b29041ba74981110b1f929a68d27f707253adff3
kawre <69250723+kawre@users.noreply.github.com>
2022-12-02 01:50:07 +0100

    fix

--- src/Day1.java
+++ src/Day1.java
@@ -3,12 +3,33 @@ import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class day1 {
-    static File file = new File("./src/inputs/day1");
+public class Day1 {
+    static File file = new File("./data/day1");
     static int[] elf_inventory = new int[0];
+
     static Elf[] elves = new Elf[0];
 
+    static class Elf {
+        int totalCalories;
+        int[] inventory;
 
+        public Elf(int[] elf_inventory) {
+            inventory = elf_inventory;
+            for (int n : inventory)
+                totalCalories += n;
+        }
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        toArray();
+        int max = findMaxTotalCalories();
+        System.out.println(max);
+        int topThree = sumOfTopThreeMaxTotalCalories();
+        System.out.println(topThree);
+    }
+
+
+    // Elf carrying the most calories
     static int findMaxTotalCalories() {
         int maxTotalCalories = 0;
         for (Elf elf : elves) {
@@ -20,7 +41,8 @@ public class day1 {
         return maxTotalCalories;
     }
 
-    static int topThreeCalories() {
+    // Sum of the top three elves carrying the most calories
+    static int sumOfTopThreeMaxTotalCalories() {
         int sum = 0;
         int[] tab = new int[0];
 
@@ -49,6 +71,7 @@ public class day1 {
         return sum;
     }
 
+    // expand elves
     static void insert(Elf elf) {
         Elf[] tmp = elves;
         elves = new Elf[tmp.length + 1];
@@ -56,6 +79,7 @@ public class day1 {
         elves[elves.length - 1] = elf;
     }
 
+    // expand elf inventory
     static void insert(int n) {
         int[] tmp = elf_inventory;
         elf_inventory = new int[tmp.length + 1];
@@ -63,41 +87,19 @@ public class day1 {
         elf_inventory[elf_inventory.length - 1] = n;
     }
 
+    // Split lines into array of calories being carried by single elf
     static void toArray() throws FileNotFoundException {
         Scanner scan = new Scanner(file);
-        String line;
 
-        while (scan.hasNextLine()) {
-            if ((line = scan.nextLine()).equals("")) {
+        do {
+            String line = scan.nextLine();
+
+            if (line.equals("")) {
                 insert(new Elf(elf_inventory));
                 elf_inventory = new int[0];
             } else {
                 insert(Integer.parseInt(line));
             }
-        }
-        insert(new Elf(elf_inventory));
-    }
-
-    public static void main(String[] args) throws FileNotFoundException {
-        toArray();
-        int max = findMaxTotalCalories();
-        System.out.println(max);
-        int topThree = topThreeCalories();
-        System.out.println(topThree);
-    }
-}
-
-class Elf {
-    int totalCalories;
-    int[] inventory;
-
-    public Elf(int[] elf_inventory) {
-        inventory = elf_inventory;
-        for (int n : inventory) {
-            totalCalories += n;
-        }
-    }
-
-    public static void main(String[] args) {
+        } while (scan.hasNextLine());
     }
 }

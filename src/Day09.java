839f43bbcc523ccad0cdd17fc3bfe14b6ab0657d
kawre <69250723+kawre@users.noreply.github.com>
2022-12-10 01:59:32 +0100

    Day 9: Part Two

--- src/Day09.java
+++ src/Day09.java
@@ -1,121 +1,119 @@
+import java.awt.*;
 import java.io.FileNotFoundException;
 import java.util.*;
+import java.util.List;
 
 public class Day09 {
     static List<String> lines;
-    static List<List<Character>> grid = new ArrayList<>();
-    static int[] s = {0, 0}, H = {0, 0}, T = {0, 0};
-    static Set<String> tailTraits = new HashSet<>();
+    static Set<Point> traits = new HashSet<>();
+    static List<Point> snake = new ArrayList<>();
 
     public static void main(String[] args) throws FileNotFoundException {
         lines = Utils.readLines("./data/day09");
-        List<Character> tmp = new ArrayList<>();
-        tmp.add('.');
-        grid.add(tmp);
-        tailTraits.add("0 0");
+        traits.add(new Point(0, 0));
+
+        for (int i = 0; i < 10; i++) {
+            snake.add(new Point(0, 0));
+        }
 
         for (String line : lines) {
             readMove(line);
         }
 
-        System.out.println(tailTraits.size());
-//        printTraits();
+        printTraits();
+        System.out.println("size: " + traits.size());
     }
 
     static void readMove(String line) {
-        String[] split = line.split(" ");
-        char move = split[0].charAt(0);
-        int amount = Integer.parseInt(split[1]);
+        String[] parts = line.split(" ");
+        int amount = Integer.parseInt(parts[1]);
+        String move = parts[0];
 
-//        System.out.println("== " + line + " ==");
         while (amount > 0) {
             switch (move) {
-                case 'U' -> {
-                    moveHead(0, 1);
-                    checkForOverflow(move);
-                }
-                case 'R' -> {
-                    moveHead(1, 1);
-                    checkForOverflow(move);
-                }
-                case 'D' -> {
-                    moveHead(0, -1);
-                }
-                case 'L' -> {
-                    moveHead(1, -1);
-                }
+                case "U" -> moveSnake(Direction.UP);
+                case "R" -> moveSnake(Direction.RIGHT);
+                case "D" -> moveSnake(Direction.DOWN);
+                case "L" -> moveSnake(Direction.LEFT);
             }
-//            printState();
             amount--;
         }
-//        System.out.println();
     }
 
-    static void moveHead(int i, int val) {
-        int[] tmp = H.clone();
-        tmp[i] += val;
-
-        double x = T[0] - tmp[0], y = T[1] - tmp[1];
-        double distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
-
-        if (distance > Math.sqrt(2)) {
-            T = H.clone();
-            String trait = T[0] + " " + T[1];
-            tailTraits.add(trait);
+    static void moveSnake(Direction dir) {
+        Point head = snake.get(0);
+        switch (dir) {
+            case UP -> head.move(head.x, head.y + 1);
+            case RIGHT -> head.move(head.x + 1, head.y);
+            case DOWN -> head.move(head.x, head.y - 1);
+            case LEFT -> head.move(head.x - 1, head.y);
         }
+        snake.set(0, head);
 
-        H = tmp;
-    }
+        for (int i = 1; i < snake.size(); i++) {
+            head = snake.get(i - 1);
+            Point tail = snake.get(i);
 
-    static void checkForOverflow(char move) {
-        if (move == 'U' || move == 'R') {
-            if (grid.size() <= H[0]) {
-                grid.add(grid.get(0));
-            }
-            if (grid.get(0).size() <= H[1]) {
-                grid.get(0).add('.');
-            }
-        }
-    }
+            if (head.distance(tail) > Math.sqrt(2)) {
+                Double minDist = Double.MAX_VALUE;
+                Point closest = head;
+                Point tmp;
 
-    static void printState() {
-        for (int i = grid.size() - 1; i >= 0; i--) {
-            List<Character> g = grid.get(i);
-            for (int j = 0; j < g.size(); j++) {
-                if (i == H[0] && j == H[1]) {
-                    System.out.print('H');
-                } else if (i == T[0] && j == T[1]) {
-                    System.out.print('T');
-
-                } else if (i == s[0] && j == s[1]) {
-                    System.out.print('s');
-                } else {
-                    System.out.print('.');
+                // East
+                tmp = new Point(head.x + 1, head.y);
+                if (tmp.distance(tail) < minDist) {
+                    minDist = tmp.distance(tail);
+                    closest = tmp;
                 }
-                System.out.print(' ');
+
+                // West
+                tmp = new Point(head.x - 1, head.y);
+                if (tmp.distance(tail) < minDist) {
+                    minDist = tmp.distance(tail);
+                    closest = tmp;
+                }
+
+                // North
+                tmp = new Point(head.x, head.y + 1);
+                if (tmp.distance(tail) < minDist) {
+                    minDist = tmp.distance(tail);
+                    closest = tmp;
+                }
+
+                // South
+                tmp = new Point(head.x, head.y - 1);
+                if (tmp.distance(tail) < minDist) {
+                    minDist = tmp.distance(tail);
+                    closest = tmp;
+                }
+
+                snake.set(i, closest);
+                if (i == snake.size() - 1) {
+                    traits.add(closest);
+                }
             }
-            System.out.println();
         }
-        System.out.println();
     }
 
     static void printTraits() {
-        for (int i = grid.size() - 1; i >= 0; i--) {
-            for (int j = 0; j < grid.get(i).size(); j++) {
-                String xd = i + " " + j;
-                if (tailTraits.contains(xd)) {
-                    if (i == 0 && j == 0) {
-                        System.out.print('s');
-                    } else
-                        System.out.print('#');
-                } else {
-                    System.out.print('.');
-                }
-                System.out.print(' ');
+        int max_x = traits.stream().max(Comparator.comparing(Point::getX)).get().x;
+        int min_x = traits.stream().min(Comparator.comparing(Point::getX)).get().x;
+        int max_y = traits.stream().max(Comparator.comparing(Point::getY)).get().y;
+        int min_y = traits.stream().min(Comparator.comparing(Point::getY)).get().y;
 
+        for (int i = max_y; i >= min_y; i--) {
+            for (int j = min_x; j <= max_x; j++) {
+                Point p = new Point(j, i);
+                System.out.print(traits.contains(p) ? "#" : ".");
             }
             System.out.println();
         }
+    }
 
+    private enum Direction {
+        UP,
+        RIGHT,
+        DOWN,
+        LEFT
     }
 }
